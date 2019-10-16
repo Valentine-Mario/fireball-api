34d856f8db9c5690833fcde74c1268fadce471da
valentine-mario <valentine13400@gmail.com>
2019-10-16 12:28:53 +0100

    added job queue to video delete using active jobs and sideik

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -121,18 +121,13 @@ class UserController < ApplicationController
     def getNotificationVideo
         @video_notification= VideoNotification.where(user_id:@current_user.id).order("created_at DESC")
         render :json=>{code:"00", message:@video_notification, unviewed:VideoNotification.where(user_id:@current_user.id, viewed:false).length + PodcastNotification.where(user_id:@current_user.id, viewed:false).length }.to_json(:include=>[:video]), status: :ok
-        for i in VideoNotification.where(user_id:@current_user.id, viewed:false) do
-            i.update(setTrue)
-        end
-        
+        VideoNotification.where(user_id:@current_user.id, viewed:false).update_all(viewed: true) 
     end
 
     def getNotificationPodcast
         @podcast_notification= PodcastNotification.where(user_id:@current_user.id).order("created_at DESC")
         render :json=>{code:"00", message:@podcast_notification}.to_json(:include=>[:podcast]), status: :ok
-        for j in PodcastNotification.where(user_id:@current_user.id, viewed:false) do
-            j.update(setTrue)
-        end
+        PodcastNotification.where(user_id:@current_user.id, viewed:false).update_all(viewed: true)   
     end
 
 
@@ -165,11 +160,6 @@ def resetPassword
     @a=(0...8).map { (65 + rand(26)).chr }.join
     defaults={password:@a}
     params.permit(:password).reverse_merge(defaults)
-end
-
-def setTrue
-    defaults = { viewed: true }
-    params.permit(:viewed).reverse_merge(defaults)
 end
 
 end
